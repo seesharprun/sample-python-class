0b06a323725799eab68771732443847a83c19e78
Sidney Andrews <sidandrews@microsoft.com>
2023-11-15 12:50:46 -0500

    Initial commit

---
+++ assignments/module-01/app.py
@@ -0,0 +1,8 @@
+left = 2
+right = 3
+
+# Add the two numbers together
+# Store the result in a variable named "sum"
+
+
+print("SUM: " + sum)
---
+++ assignments/module-02/app.py
@@ -0,0 +1,5 @@
+indicator = 5
+
+# Check to see if the indicator is odd or even
+# If it's odd, print "ODD"
+# If it's even, print "EVEN"
